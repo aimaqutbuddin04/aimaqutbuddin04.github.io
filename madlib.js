45805946a85b7fc1d963a4d46e8d3c519c7f1c1a
aimaqutbuddin04 <43216133+aimaqutbuddin04@users.noreply.github.com>
2018-11-29 13:00:25 -0600

    Add files via upload

---
+++ madlib.js
@@ -0,0 +1,25 @@
+
+//Shreya Patel and Aiman Zainab helped me with JavaScript
+function array1()
+{
+    var words = [];
+    words.push(document.madLibs.word1.value);
+    words.push(document.madLibs.word2.value);
+    words.push(document.madLibs.word3.value);
+    words.push(document.madLibs.word4.value);
+    words.push(document.madLibs.word5.value);
+    words.push(document.madLibs.word6.value);
+    words.push(document.madLibs.word7.value);
+    words.push(document.madLibs.word8.value);
+    words.push(document.madLibs.word9.value);
+    words.push(document.madLibs.word10.value);
+    words.push(document.madLibs.word11.value);
+    words.push(document.madLibs.word12.value);
+    words.push(document.madLibs.word13.value);
+    words.push(document.madLibs.word14.value);
+    return " I enjoy using my imagination to write stories about " + words[0].bold() + ",or " + words[1].bold() + ". I can write about people who are "
+        + words[9].bold()+ " feet tall, with " + words[10].bold() + " hair,who can " + words[6].bold() + ". When I write, I can be silly or " + words[12].bold() +
+        ". Making up stories about living in " + words[11].bold() + ", or being a " + words[13].bold() + ", where people can " + words[7].bold() + " is so much fun. There are many types of writing, like poetry, "
++ words[2].bold()+ ",and " + words[3].bold()+ ". I'd like to be a writer when I grow up, and write " + words[4].bold() + " for children about " + words[8].bold()+ " in " + words[5].bold()+ "."
+}
+//found from internet
